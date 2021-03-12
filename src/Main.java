1b87c2184000e508fa6dd7c76048c113ec29e209
AdrianBudnik <adrbud000@utp.edu.pl>
2021-03-12 12:48:22 +0100

    Cwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -18,10 +20,14 @@ public class Main {
         Date dateToEnd4 = new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(90));
         Task task4 = new Task("New", "Some task to do", TaskCategory.NEW, dateToEnd4.toString());
 
-        System.out.println(task1);
-        System.out.println(task2);
-        System.out.println(task3);
+        List<Task> taskList = new ArrayList<Task>();
+        taskList.add(task1);
+        taskList.add(task2);
+        taskList.add(task3);
+        taskList.add(task4);
 
-        System.out.println(task4.toString());
+        for (Task task : taskList) {
+            System.out.println(task.toString());
+        }
     }
 }
