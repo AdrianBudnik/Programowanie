760e199776939607c8564ca62dc540653646767b
AdrianBudnik <adrbud000@utp.edu.pl>
2021-03-12 12:14:17 +0100

    Cwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date dateToEnd1 = new Date(System.currentTimeMillis() + 3600000);
+        Date dateToEnd2 = new Date(System.currentTimeMillis() + 86400000);
+        Date dateToEnd3 = new Date(System.currentTimeMillis() + 604800000);
+
+        Task task1 = new Task("Printing", "Printing documents", TaskCategory.IN_PROGRESS, dateToEnd1.toString());
+        Task task2 = new Task("Measuring", "Measuring temperature", TaskCategory.NEW, dateToEnd2.toString());
+        Task task3 = new Task("Waiting", "Waiting for new tasks", TaskCategory.DONE, dateToEnd3.toString());
+
+        System.out.println(task1);
+        System.out.println(task2);
+        System.out.println(task3);
     }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,7 +1,22 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.lang.System;
+
 public class Task {
     String taskName;
     String taskDescription;
     TaskCategory taskCategory;
     String taskDateTimeStart;
     String taskDateTimeStop;
+
+    public Task(String taskName, String taskDescription, TaskCategory taskCategory, String taskDateTimeStop) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskCategory = taskCategory;
+        this.taskDateTimeStop = taskDateTimeStop;
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        taskDateTimeStart = date.toString();
+    }
 }
