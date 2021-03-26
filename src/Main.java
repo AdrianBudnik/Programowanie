9bebe4bc566e4947ee569ea7f19e2be200d0ac25
AdrianBudnik <adrbud000@utp.edu.pl>
2021-03-26 12:15:40 +0100

    Cwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -13,21 +13,12 @@ public class Main {
         Date dateToEnd2 = new Date(System.currentTimeMillis() + 86400000);
         Date dateToEnd3 = new Date(System.currentTimeMillis() + 604800000);
 
+        Task subtask1 = new Subtask("Sub Printing","Printing", "Printing documents", TaskCategory.IN_PROGRESS, dateToEnd1.toString());
+        Task subtask2 = new Subtask("Sub Measuring","Measuring", "Measuring temperature", TaskCategory.NEW, dateToEnd2.toString());
+        Task subtask3 = new Subtask("Sub Waiting", "Waiting", "Waiting for new tasks", TaskCategory.DONE, dateToEnd3.toString());
+
         Task task1 = new Task("Printing", "Printing documents", TaskCategory.IN_PROGRESS, dateToEnd1.toString());
         Task task2 = new Task("Measuring", "Measuring temperature", TaskCategory.NEW, dateToEnd2.toString());
         Task task3 = new Task("Waiting", "Waiting for new tasks", TaskCategory.DONE, dateToEnd3.toString());
-
-        Date dateToEnd4 = new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(90));
-        Task task4 = new Task("New", "Some task to do", TaskCategory.NEW, dateToEnd4.toString());
-
-        List<Task> taskList = new ArrayList<Task>();
-        taskList.add(task1);
-        taskList.add(task2);
-        taskList.add(task3);
-        taskList.add(task4);
-
-        for (Task task : taskList) {
-            System.out.println(task.toString());
-        }
     }
 }
