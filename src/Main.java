d59d783571b0d2163813fc9143676cf167d74ac4
AdrianBudnik <adrbud000@utp.edu.pl>
2021-03-26 12:37:15 +0100

    Cwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,35 @@ public class Main {
         Object task1 = new Task("Printing", "Printing documents", TaskCategory.IN_PROGRESS, dateToEnd1.toString());
         Object task2 = new Task("Measuring", "Measuring temperature", TaskCategory.NEW, dateToEnd2.toString());
         Object task3 = new Task("Waiting", "Waiting for new tasks", TaskCategory.DONE, dateToEnd3.toString());
+
+        getDetailInfo(subtask1);
+        getDetailInfo(task3);
+    }
+
+    public static void getDetailInfo(Object o) {
+        String taskName;
+        String taskDesc;
+        String taskCategory;
+        String taskDateTimeStart;
+        String taskDateTimeStop;
+        String subtaskTitle = "1";
+        if (((Task) o).getTaskName().isEmpty()) {taskName = "B/D";} else {taskName = ((Task) o).getTaskName();}
+        if (((Task) o).getTaskDescription().isEmpty()) {taskDesc = "B/D";} else {taskDesc = ((Task) o).getTaskDescription();}
+        if (((Task) o).getTaskCategory().toString().isEmpty()) {taskCategory = "B/D";} else {taskCategory = ((Task) o).getTaskCategory().toString();}
+        if (((Task) o).getTaskDateTimeStart().isEmpty()) {taskDateTimeStart = "B/D";} else {taskDateTimeStart = ((Task) o).getTaskDateTimeStart();}
+        if (((Task) o).getTaskDateTimeStop().isEmpty()) {taskDateTimeStop = "B/D";} else {taskDateTimeStop = ((Task) o).getTaskDateTimeStop();}
+        try {
+            if (((Subtask) o).getSubtaskName().isEmpty()) {subtaskTitle = "B/D";} else {subtaskTitle = ((Subtask) o).getSubtaskName();}
+        }
+        catch(Exception e) {
+            subtaskTitle = "B/D";
+        }
+
+        System.out.format("| taskName | wartość | %s\n", taskName);
+        System.out.format("| taskDescription | wartość | %s\n", taskDesc);
+        System.out.format("| taskCategory | wartość | %s\n", taskCategory);
+        System.out.format("| taskDateTimeStart | wartość | %s\n", taskDateTimeStart);
+        System.out.format("| taskDateTimeStop | wartość | %s\n", taskDateTimeStop);
+        System.out.format("| subtaskTitle | wartość | %s\n\n", subtaskTitle);
     }
 }
